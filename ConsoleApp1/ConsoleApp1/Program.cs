190f02dff680ea8102d64d1b65cd330da004ac3f
alliyahdevera <alliyahalceldevera@gmail.com>
2026-06-15 09:35:34 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+namespace StudentManagementSystem
+{
+    class Student
+    {
+        public string name {  get; set; }
+        public List<int> Grades { get; set; }
+
+        public Student(string name, List<int> grades)
+        {
+            Name = name;
+            Grades = grades;
+        }
+
+        public double GetAverage()
+        {
+            if (Grades.Count == 0) return 0;
+            double sum = 0;
+            foreach (int grade in Grades) 
+            { 
+                sum += grade; 
+            } 
+            return sum / Grades.Count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
