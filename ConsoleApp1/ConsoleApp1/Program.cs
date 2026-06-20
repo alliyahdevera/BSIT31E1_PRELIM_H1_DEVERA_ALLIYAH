7d1472fe60153a3f6bf5f9e64650f34395e40ba4
alliyahdevera <alliyahalceldevera@gmail.com>
2026-06-20 10:04:32 +0800

    FINAL

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,44 @@
-﻿    class Program
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagementSystem
+{
+    class Student
+    {
+        private string name;
+        private List<int> grades;
+
+        public Student(string studentName, List<int> studentGrades)
+        {
+            name = studentName;
+            grades = studentGrades;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public List<int> GetGrades()
+        {
+            return grades;
+        }
+
+        public double GetAverage()
+        {
+            if (grades.Count == 0)
+                return 0;
+
+            double sum = 0;
+
+            foreach (int grade in grades)
+            {
+                sum += grade;
+            }
+
+            return sum / grades.Count;
+        }
+        class Program
 
     {
 
@@ -277,44 +317,5 @@
         }
 
     }
-﻿using System;
-using System.Collections.Generic;
-    
-namespace StudentManagementSystem
-{
-    class Student
-    {
-        private string name;
-        private List<int> grades;
 
-        public Student(string studentName, List<int> studentGrades)
-        {
-            name = studentName;
-            grades = studentGrades;
-        }
-
-        public string GetName()
-        {
-            return name;
-        }
-
-        public List<int> GetGrades() 
-        {
-            return grades;
-        }
-
-        public double GetAverage()
-        {
-            if (grades.Count == 0)
-                return 0;
-
-            double sum = 0;
-
-            foreach (int grade in grades)
-            {
-                sum += grade;
-            }
-
-            return sum / grades.Count;
-        }
 }
