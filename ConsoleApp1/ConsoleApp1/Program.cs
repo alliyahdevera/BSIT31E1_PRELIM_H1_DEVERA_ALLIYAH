5d9d7ccf2107577804930f9c73eb7e6fbca49b66
SolisSophiaCassandra <c1395-24@itmlyceumalabang.onmicrosoft.com>
2026-06-20 09:33:54 +0800

    program cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,36 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿    
 
-namespace StudentManagementSystem
-{
-    class Student
-    {
-        public string Name { get; set; }
-        public List<int> Grades { get; set; }
-
-        public Student(string name, List<int> grades)
-        {
-            Name = name;
-            Grades = grades;
-        }
-
-        public double GetAverage()
-        {
-            if (Grades.Count == 0)
-                return 0;
-
-            double sum = 0;
-
-            foreach (int grade in Grades)
-            {
-                sum += grade;
-            }
-
-            return sum / Grades.Count;
-        }
-    }
-
-    class Program
+class Program
     {
         static List<Student> students = new List<Student>();
 
@@ -197,5 +167,4 @@ namespace StudentManagementSystem
             Console.WriteLine($"Highest Grade: {highestGrade}\n");
         }
 
-    }
 }
